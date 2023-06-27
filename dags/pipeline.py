8935a3a4c5aadb302bb8417796d954da7f079ed0
4sitescarp@gmail.com <4sitescarp@gmail.com>
2023-06-27 20:30:51 +0300

    Add Airflow

---
+++ dags/pipeline.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from tasks import *
+
+with DAG(
+    dag_id='first_sample_dag',
+    start_date=datetime(2022, 5, 28),
+    schedule_interval=None
+) as dag:
+    preprocessing_task = PythonOperator(
+        task_id='preprocessing_task',
+        python_callable=preprocessing
+    )
+
+    core_nlp_task = PythonOperator(
+        task_id='core_nlp',
+        python_callable=core_nlp
+    )
+
+    postprocessing_task = PythonOperator(
+        task_id='postprocessing',
+        python_callable=postprocessing
+    )
+
+preprocessing_task >> core_nlp_task >> postprocessing_task
+
